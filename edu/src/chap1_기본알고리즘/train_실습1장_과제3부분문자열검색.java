e182d04d46ddf4ee4be45258b1f9f54912ec0aed
nx2803 <nx2803@gmail.com>
2025-09-17 18:00:41 +0900

    v11

---
+++ edu/src/chap1_기본알고리즘/train_실습1장_과제3부분문자열검색.java
@@ -0,0 +1,54 @@
+package chap1_기본알고리즘;
+
+import java.util.ArrayList;
+
+/*
+ * 주어진 긴 문자열 text에서 패턴 문자열 pattern이 등장하는 모든 시작 인덱스를 찾으세요.
+ *
+ * 입력: text = "abxabcabcabyabcaby", pattern = "abcaby"
+ * 출력: [6,12]
+ * 
+ * 패턴이 나타나는 인덱스를 반환
+ */
+public class train_실습1장_과제3부분문자열검색 {
+
+	public static void main(String[] args) {
+		String text = "ababcabcabababd";
+		String pattern = "ababd";
+
+		searchSubstring(text, pattern);
+		text = "abxabcabcabyabcaby";
+		pattern = "abcaby";
+		searchSubstring(text, pattern);
+	}
+
+	private static void searchSubstring(String text, String pattern) {
+		// TODO 자동 생성된 메소드 스텁
+		ArrayList<Integer> res = new ArrayList<Integer>();
+		String[] t = text.split("");
+		String[] p = pattern.split("");
+
+		boolean ox = false;
+		int j = 0;
+		for (int i = 0; i <= t.length - p.length; i++) {
+			for (j = 0; j < p.length; j++) {
+				if (!t[i + j].equals(p[j])) {
+					break;
+				}
+			}
+			if (j == p.length) {
+				res.add(i);
+				ox = true;
+			}
+		}
+		System.out.println("Text : " + text);
+		System.out.println("Pattern : " + pattern);
+		if (!ox) {
+			System.out.println("패턴 발견 안됨");
+		} else {
+			System.out.println("패턴이 나타난 인덱스 : " + res);
+
+		}
+
+	}
+}
---
+++ edu/src/com/ruby/java/ch13/boundGen/BagTest.java
@@ -0,0 +1,59 @@
+package com.ruby.java.ch13.boundGen;
+
+class Bag<T extends Solid>{
+	private T thing;
+	private String owner;
+	
+	public Bag(T thing) {
+		this.thing=thing;
+	}
+	
+	public T getThing() {
+		return thing;
+	}
+	
+	public void setThing(T thing) {
+		this.thing=thing;
+	}
+	void showType() {
+		System.out.println("T의 타입은 "+thing.getClass().getName());
+	}
+	
+	public String getOwner() {
+		return owner;
+	}
+	public void setOwner(String owner) {
+		this.owner=owner;
+	} 
+	boolean isSameOwner(Bag<?> obj) {
+		if (this.owner.equals(obj.getOwner())) {
+			return true;
+		}
+		return false;
+	}
+}
+
+class Solid{}
+class Liquid{}
+class Book extends Solid{}
+class PencilCase extends Solid{}
+class Notebook extends Solid{}
+
+
+
+public class BagTest {
+
+	public static void main(String[] args) {
+		// TODO 자동 생성된 메소드 스텁
+		Bag<Book> bag = new Bag<>(new Book());
+		Bag<PencilCase> bag2 = new Bag<>(new PencilCase());
+
+		bag.setOwner("김푸름");
+		bag2.setOwner("김푸름");
+		
+		boolean result = bag.isSameOwner(bag2);
+		if(result) System.out.println("소유자가 동일합니다.");
+		else System.out.println("소유자가 다릅니다..");
+	}	
+
+}
