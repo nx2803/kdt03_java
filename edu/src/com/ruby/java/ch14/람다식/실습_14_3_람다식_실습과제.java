539372d975a56b893c267523e539f952ba541244
nx2803 <nx2803@gmail.com>
2025-09-19 18:00:28 +0900

    v13.5

--- edu/src/com/ruby/java/ch14/람다식/실습_14_3_람다식_실습과제.java
+++ edu/src/com/ruby/java/ch14/람다식/실습_14_3_람다식_실습과제.java
@@ -30,6 +30,7 @@ class UnderflowException extends RuntimeException {
 
 interface MediaBook {
 	public String getTitle();
+
 	public String getISBN();
 }
 
@@ -39,7 +40,7 @@ class Book implements MediaBook, Comparable<Book> {
 	private String author;
 	private int publicationYear;
 	private String isbn;
-	
+
 	// 생성자
 	public Book(String title, String author, int publicationYear, String isbn) {
 		this.title = title;
@@ -47,24 +48,27 @@ class Book implements MediaBook, Comparable<Book> {
 		this.publicationYear = publicationYear;
 		this.isbn = isbn;
 	}
-	
+
 	@Override
 	public int compareTo(Book o) {
 		return title.compareTo(o.title);
 	}
+
 	@Override
 	public String getTitle() {
 		return title;
 	}
+
 	@Override
 	public String getISBN() {
 		return isbn;
 	}
+
 	@Override
 	public String toString() {
 		return "Book [title=" + title + ", author=" + author + ", publicationYear=" + publicationYear + ", isbn=" + isbn
 				+ "]";
-	}	
+	}
 }
 
 // CDBook 클래스(title : cdStory, isbn : catalogNumber)
@@ -72,29 +76,32 @@ class CDBook implements MediaBook, Comparable<CDBook> {
 	private String cdStory;
 	private String artist;
 	private String catalogNumber;
-	
+
 	public CDBook(String cdStory, String artist, String catalogNumber) {
 		this.cdStory = cdStory;
 		this.artist = artist;
 		this.catalogNumber = catalogNumber;
 	}
-	
+
 	@Override
 	public int compareTo(CDBook o) {
 		return cdStory.compareTo(o.cdStory);
 	}
+
 	@Override
 	public String getTitle() {
 		return cdStory;
 	}
+
 	@Override
 	public String getISBN() {
 		return catalogNumber;
 	}
+
 	@Override
 	public String toString() {
 		return "CDBook [cdStory=" + cdStory + ", artist=" + artist + ", catalogNumber=" + catalogNumber + "]";
-	}	
+	}
 }
 
 // USBBook 클래스(title : usbName, isbn : serialNumber)
@@ -102,25 +109,28 @@ class USBBook implements MediaBook, Comparable<USBBook> {
 	private String usbName;
 	private int capacity;
 	private String serialNumber;
-	
+
 	public USBBook(String usbName, int capacity, String serialNumber) {
 		this.usbName = usbName;
 		this.capacity = capacity;
 		this.serialNumber = serialNumber;
 	}
-	
+
 	@Override
 	public int compareTo(USBBook o) {
 		return usbName.compareTo(o.usbName);
 	}
+
 	@Override
 	public String getTitle() {
 		return usbName;
 	}
+
 	@Override
 	public String getISBN() {
 		return serialNumber;
 	}
+
 	@Override
 	public String toString() {
 		return "USBBook [usbName=" + usbName + ", capacity=" + capacity + ", serialNumber=" + serialNumber + "]";
@@ -160,7 +170,7 @@ class Library<T extends MediaBook> {
 	// 제목으로 정렬
 	public void sortBooksByTitle() {
 		// 익명 클래스로 구현
-		Comparator<MediaBook> compTitle = new Comparator<MediaBook>(){
+		Comparator<MediaBook> compTitle = new Comparator<MediaBook>() {
 			@Override
 			public int compare(MediaBook s1, MediaBook s2) {
 				return (s1.getTitle().compareTo(s2.getTitle()));
@@ -172,7 +182,7 @@ class Library<T extends MediaBook> {
 	// ISBN으로 정렬
 	public void sortBooksByISBN() {
 		// 람다식으로 구현
-		Collections.sort(items, (s1, s2)->s1.getISBN().compareTo(s2.getISBN()));
+		Collections.sort(items, (s1, s2) -> s1.getISBN().compareTo(s2.getISBN()));
 	}
 
 	// 책 검색
@@ -186,54 +196,51 @@ class Library<T extends MediaBook> {
 		return null;
 	}
 }
+
 public class 실습_14_3_람다식_실습과제 {
 	public static void main(String[] args) {
 		Library<MediaBook> library = new Library<>();
-		List<MediaBook> books = List.of(
-			new Book("자바", "강감찬", 1995, "12024"),
-			new CDBook("파이썬", "이순신", "22023"),
-			new Book("C#", "을지문덕", 2008, "325"),
-			new Book("자료구조", "연개소문", 1994, "42"),
-			new USBBook("리액트", 2000, "52007"),
-			new USBBook("C/C++", 200, "62000")
-		);
-		
+		List<MediaBook> books = List.of(new Book("자바", "강감찬", 1995, "12024"), new CDBook("파이썬", "이순신", "22023"),
+				new Book("C#", "을지문덕", 2008, "325"), new Book("자료구조", "연개소문", 1994, "42"),
+				new USBBook("리액트", 2000, "52007"), new USBBook("C/C++", 200, "62000"));
+
 		// 예외 처리를 적용한 책 추가
-        System.out.println("\n>>>책 추가");
-        // forEach 람다식으로 구현
-        try {
-            books.forEach(book -> library.addBook(book));
-        } catch(OverflowException e) {
-            System.out.println(e.getMessage());
-        }
-	
+		System.out.println("\n>>>책 추가");
+		// forEach 람다식으로 구현
+		books.forEach(book -> {
+			try {
+				library.addBook(book);
+			} catch (OverflowException e) {
+				System.out.println(e.getMessage());
+			}
+		});
 
 		// 도서 목록 출력
-        library.printBooks("\n>>>현재 도서 목록:");
+		library.printBooks("\n>>>현재 도서 목록:");
 
-        // 제목 기준 정렬 및 출력
-        library.sortBooksByTitle();
-        library.printBooks("\n>>>제목 정렬 후:");
+		// 제목 기준 정렬 및 출력
+		library.sortBooksByTitle();
+		library.printBooks("\n>>>제목 정렬 후:");
 
-        // ISBN 기준 정렬 및 출력
-        library.sortBooksByISBN();
-        library.printBooks("\n>>>ISBN 정렬 후:");
+		// ISBN 기준 정렬 및 출력
+		library.sortBooksByISBN();
+		library.printBooks("\n>>>ISBN 정렬 후:");
 
-        // Title로 검색 및 출력
-        String title = "자료구조";
-        System.out.println("\n>>>Title[" + title + "]로 검색");
-        System.out.println("검색 결과 : " + library.searchBookByTitle(title));
-        
+		// Title로 검색 및 출력
+		String title = "자료구조";
+		System.out.println("\n>>>Title[" + title + "]로 검색");
+		System.out.println("검색 결과 : " + library.searchBookByTitle(title));
+
 		// 예외 처리를 적용한 책 삭제
-        System.out.println("\n>>>책 삭제");
-        while (true) {
-            try {
-            	library.removeBook();
-            } catch (UnderflowException e) {
-                System.out.println(e.getMessage());
-                break;
-            }
-        }
-        library.printBooks("\n>>>최종 도서 목록");
+		System.out.println("\n>>>책 삭제");
+		while (true) {
+			try {
+				library.removeBook();
+			} catch (UnderflowException e) {
+				System.out.println(e.getMessage());
+				break;
+			}
+		}
+		library.printBooks("\n>>>최종 도서 목록");
 	}
 }
